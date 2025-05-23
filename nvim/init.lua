48e077a4937ad5347c87bf3daec27e4c157c57e9
Dawid Owoc <dawidowoc6@gmail.com>
2025-05-23 23:16:10 +0200

    nvim: copy existing config from vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,7 @@
 vim.keymap.set('n', '<C-e>', ':Explore<CR>', { noremap = true, silent=true })
+vim.cmd('syntax on')
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+-- Keep cursor centered vertically when scrolling
+vim.opt.scrolloff = 999
