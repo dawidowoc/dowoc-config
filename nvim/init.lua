fd0157e2b71dc8aab78ef2d49a4d9479bbe8b8ea
Dawid Owoc <dawidowoc6@gmail.com>
2025-05-23 23:50:02 +0200

    nvim: 4 spaces for tabs

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,3 +7,6 @@ vim.opt.relativenumber = true
 vim.opt.ignorecase = true
 -- Keep cursor centered vertically when scrolling
 vim.opt.scrolloff = 999
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
